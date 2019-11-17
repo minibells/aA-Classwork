097b4f576511875e9d415d5b9392962738f66587
Lex Logan <alexie.henrietta@gmail.com>
2019-11-17 14:52:40 -0800

    Associations and Rails

---
+++ W5D5/movie_buff/app/models/casting.rb
@@ -0,0 +1,22 @@
+class Casting < ApplicationRecord
+  belongs_to :actor,
+    class_name: :Actor,
+    foreign_key: :actor_id,
+    primary_key: :id
+
+  belongs_to :movie,
+    class_name: :Movie,
+    foreign_key: :movie_id,
+    primary_key: :id
+
+end
+# == Schema Information
+#
+# Table name: castings
+#
+#  id       :integer(8)      not null, primary key
+#  actor_id :integer         not null
+#  movie_id :integer         not null
+#  ord      :integer         not null
+#
+
